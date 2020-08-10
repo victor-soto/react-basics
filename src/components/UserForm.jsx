f70bdb88d035316c232363d7a3123ef23a26ead7
Victor Soto <dev.victor.soto@gmail.com>
2020-08-10 18:58:28 -0500

    First commit

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class UserForm extends Component {
+  render() {
+    return (
+      'Formulario de usuarios'
+    );
+  }
+}
---
+++ src/components/ViewList.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class ViewList extends Component {
+  render() {
+    return (
+      'Lista de usuarios'
+    );
+  }
+}
